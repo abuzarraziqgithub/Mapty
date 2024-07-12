20b4e34e38943c57a98807405e55bd1ed2b84fca
abuzarraziqgithub <abuzarraziqo@gmail.com>
2024-07-12 16:30:31 +0500

    leaflet_library-added

--- script.js
+++ script.js
@@ -10,3 +10,31 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    position => {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+
+      console.log('Latitude', latitude);
+      console.log('Longitude', longitude);
+
+      console.log(`https://www.google.com/maps/@${longitude},${latitude}`);
+
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords).addTo(map).bindPopup("Hey! I'm Here 🤞").openPopup();
+    },
+    () => {
+      alert("Couldn't get the Location ):");
+    }
+  );
+}
